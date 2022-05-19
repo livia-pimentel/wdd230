6dd44ff4360001d7c38718e0457cebbf5a3d5b8d
livia-pimentel <livia_macedo89@yahoo.com.br>
2022-05-19 09:00:01 -0300

    Add week 05 activity - Chamber Home Page Continued

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -17,3 +17,19 @@ x.onclick = toggleMenu;
 
 //Last Modifition
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+
+//Banner colocar dia 1 ou 2
+
+let d = new Date().getDay();
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 2) { //teste
+    banner.style.display = "block";
+} 
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+    banner.style.display = "none";
+})
