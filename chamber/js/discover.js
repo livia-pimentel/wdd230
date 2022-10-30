4a70d3cbc44c5cb90a1cdbda24361d85ed9008df
livia-pimentel <livia_macedo89@yahoo.com.br>
2022-10-30 01:19:48 -0300

    Comments in //Data Storage

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -18,8 +18,12 @@ numVisits++;
 localStorage.setItem('visits-ls', numVisits);  
 
 
-//Data Storage
 
+//Data Storage
+// Com funções elas só serão lidas quando chamadas.
+// Storage só recebe String, new Date() é uma objeto e não uma string
+
+// Com essa função vou conseguir formatar o new Date() que é um objeto em String e determinar seu formato
 function currentDate(){
   const time = new Date()
   let day = String(time.getDate()).padStart(2, '0');
@@ -29,6 +33,7 @@ function currentDate(){
   return now;
 }
 
+//Uma vez transformando new Date() em String verifico se já exite uma variavel no localStorage, se não tiver irá ser criada uma
 let date = currentDate();
 if(window.localStorage.getItem('lastDay') == undefined){
   localStorage.setItem('lastDay', date)
@@ -37,18 +42,23 @@ if(window.localStorage.getItem('lastDay') == undefined){
 function calculateDays(today, last){
   let date1 = new Date(today);
   let date2 = new Date(last);
+  //Converte para objetos
   const difference_time = Math.abs(date1 - date2);
+  //Calcula a diferença
   const difference_days = Math.ceil(difference_time / (1000 * 60 * 60 * 24));
   return difference_days
 }
 
+//Faz o cálculo utilizando o localStorage
 let finalDay = calculateDays(window.localStorage.getItem('lastDay'), date);
-// alert(finalDay)
+//Imprime no HTML
 const numberDays = document.querySelector('.number_days')
 numberDays.textContent = finalDay
 
+
 // Last Modification
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
+
 
 //IntersectionObserver for Images
 //get all imgs with data-src attribute
