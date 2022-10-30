2702a79854994d0b2b67972989af916da28591a0
livia-pimentel <livia_macedo89@yahoo.com.br>
2022-10-29 21:45:36 -0300

    Try to make localStorage to date

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -18,6 +18,34 @@ numVisits++;
 localStorage.setItem('visits-ls', numVisits);  
 
 
+//Data storage
+today = new Date()
+
+
+const numberDays = document.querySelector('.number_days')
+// localStorage.setItem('days', new Date())
+
+
+let date_1 = new Date('10/25/2022');
+// let date_1 = localStorage.getItem('days');
+
+let date_2 = today;
+
+const days = (date_1, date_2) =>{
+    let difference = Math.abs(date_2.getTime() - date_1.getTime());
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+numberDays.innerHTML = days(date_1, date_2)
+
+
+
+
+// Last Modification
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
+
+
+
 //IntersectionObserver for Images
 //get all imgs with data-src attribute
 const imagesToLoad = document.querySelectorAll('img[data-src]');
@@ -25,7 +53,7 @@ const imagesToLoad = document.querySelectorAll('img[data-src]');
 //optional parameters being set for the IntersectionObserver
 const imgOptions = {
     threshold: 1,
-    rootMargin: '0px 0px 10px 0px'
+    rootMargin: '0px 0px 50px 0px'
 };
 
 const loadImages = (image) => {
@@ -55,5 +83,5 @@ if ("IntersectionObserver" in window) {
   }
 
 
-// Last Modification
-document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
+
+
