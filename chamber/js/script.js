376a4e309d8e906c7b547b0af0eb09b550d8973b
livia-pimentel <livia_macedo89@yahoo.com.br>
2022-11-04 01:14:21 -0300

    comment in script.js

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -56,6 +56,7 @@ close.addEventListener('click', () => {
 //     }
 // }
 
+//Hidden Element - Join Page
 let date = new Date();
 const hiddenElement = document.querySelector("#dateValue");
 hiddenElement.setAttribute("value",date);
