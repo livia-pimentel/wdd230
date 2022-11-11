d39fbc8bf7d41027cd4702904b11fc70b98905dd
livia-pimentel <livia_macedo89@yahoo.com.br>
2022-11-11 20:32:18 -0300

    Update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -75,3 +75,39 @@ function view(option){
   }
 }
 
+//Date Script
+const datefield = document.querySelector('.date');
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(now);
+datefield.innerHTML = `${fulldate}`
+
+//Menu Hamburger
+function toggleMenu(){
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open')
+}
+
+const x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
+
+// Last Modification
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
+
+//Banner 
+
+let bannerDay = new Date().getDay();
+
+const banner = document.querySelector('#banner');
+if (bannerDay === 1 || bannerDay === 2){
+    banner.style.display = 'block';
+} else{
+    banner.style.display = 'none';
+}
+
+const close = document.querySelector('#close');
+
+close.addEventListener('click', () => {
+    banner.style.display = 'none';
+})
+
